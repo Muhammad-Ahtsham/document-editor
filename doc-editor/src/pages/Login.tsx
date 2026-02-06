f34240c6b4a23dc814e599eb0f69fa98b184f279
Muhammad-Ahtsham <ahtsham54t@gmail.com>
2026-02-06 19:36:27 +0500

    login page  ui fix

--- doc-editor/src/pages/Login.tsx
+++ doc-editor/src/pages/Login.tsx
@@ -148,14 +148,9 @@ const Login = () => {
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-300"></div>
                 </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="px-2 bg-white text-gray-500">
-                    Or continue with
-                  </span>
-                </div>
               </div>
             </div>
-            <p className="mt-8 text-center text-sm text-gray-600">
+            <p className="mt-8 pt-3 text-center text-sm text-gray-600">
               Don't have an account?{" "}
               <a
                 href="/signup"
